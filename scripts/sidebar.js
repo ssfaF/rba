5d81e752f950f09858005713bae73d26b2a1a4a1
_ma.x.im_8 <112706383+ssfaF@users.noreply.github.com>
2024-12-21 19:15:30 +0200

    Delete scripts directory

--- scripts/sidebar.js
+++
@@ -1,6 +0,0 @@
-// sidebar
-const toggleButton = document.getElementById('toggleSidebar');
-const sidebar = document.getElementById('sidebar');
-toggleButton.addEventListener('click', () => {
-  sidebar.classList.toggle('active');
-});
